572303eb07f78dfebb3e6b5a730c3c6fdc716c7f
neiltcm <ntseachi@uwaterloo.ca>
2018-07-06 22:50:58 -0400

    First ui commit

---
+++ ui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <chat></chat>
+  </div>
+</template>
+
+<script>
+import Chat from './components/chat/Chat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ ui/src/components/chat/InputBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="input">
+    <p>INPUT WORKS</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+}
+</script>
+
+<style scoped>
+#input {
+  border: 1px solid red;
+  width: 100%;
+}
+</style>
