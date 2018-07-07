1d95f23c71d363bb34e284989b8f0f6f38e331bd
elvinlkw <elvinlkw@hotmail.com>
2018-07-07 01:38:27 -0400

    added input and send button

--- ui/src/components/chat/InputBox.vue
+++ ui/src/components/chat/InputBox.vue
@@ -1,18 +1,41 @@
 <template>
-  <div id="input">
-    <p>INPUT WORKS</p>
+  <div id="input-container">
+    <div id="input-text">
+      <md-field>
+        <label>Type here!</label>
+        <md-textarea v-model="autogrow" md-autogrow></md-textarea>
+      </md-field>
+    </div>
+    <div id="input-button">
+      <md-button>Send</md-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Input',
+  data: () => ({
+    autogrow: null
+  })
 }
 </script>
 
 <style scoped>
-#input {
-  // border: 1px solid red;
+#input-container {
+  border: 1px solid red;
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
+
+#input-text {
+}
+
+#input-button {
+  align-self:center;
+
+}
+
 </style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/normalize.css';
 
-import { MdElevation } from 'vue-material/dist/components';
-import 'vue-material/dist/vue-material.min.css'
+import { MdElevation, MdField, MdButton } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
 
 Vue.use(MdElevation);
+Vue.use(MdField);
+Vue.use(MdButton);
 
 Vue.config.productionTip = false
 
