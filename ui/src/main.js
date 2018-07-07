cca01f744f32133ab56b80d576c81612970eeff8
neiltcm <ntseachi@uwaterloo.ca>
2018-07-06 23:38:41 -0400

    Added vue material dependency

--- ui/src/main.js
+++ ui/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/normalize.css';
 
+import { MdElevation } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(MdElevation);
+
 Vue.config.productionTip = false
 
 new Vue({
